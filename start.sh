45377c7e3fdfb076f389bc679bad6b5969b8d1fa
Steve Austin <steve@austin.com>
2025-08-03 16:16:15 +0100

    Added sleep to start up script

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# Start php-fpm in the background
 /usr/local/sbin/php-fpm --nodaemonize &
+
+# Wait for PHP-FPM to start properly (optional but helps avoid race conditions)
+sleep 1
+
+# Start nginx in the foreground
 nginx -g 'daemon off;'
