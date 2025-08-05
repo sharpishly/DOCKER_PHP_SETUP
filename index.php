362ee63c2ec1142dee8b790bcc242ee2efaee1ac
Steve Austin <steve@austin.com>
2025-08-05 16:57:42 +0100

    Added a under constuction page

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php
-// test
-print_r($_SERVER);
-?>
+<html>
+    <head>
+        <title>Sharpishly</title>
+        <style>
+            body {
+                font-family: Arial, Helvetica, sans-serif;
+                font-size: 1.9em;
+                color: #eee;
+                background-color: #ccc;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Sharpishly</h1>
+        <h2>Version v0.01 is nearly here</h2>
+        <p>
+            Checkout the <a href="https://github.com/sharpishly">repos</a> while your here
+        </p>
+    </body>
+</html>
